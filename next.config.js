aaca41b2e7e01b131091516a731241d97a271e12
Fabio Mangiameli <fabio.mangiameli22@gmail.com>
2025-12-21 12:40:52 +0100

    Added Next.js template

---
+++ next.config.js
@@ -0,0 +1,17 @@
+/** @type {import('next').NextConfig} */
+const nextConfig = {
+  output: 'export',
+  distDir: 'dist',
+  images: {
+    unoptimized: true,
+    remotePatterns: [
+      {
+        protocol: "https",
+        hostname: "cdn.sanity.io",
+        port: "",
+      },
+    ],
+  },
+};
+
+module.exports = nextConfig;
