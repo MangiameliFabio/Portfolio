93cc94654dac99de27cff56039fd05c6c9edf10e
Fabio Mangiameli <fabio.mangiameli22@gmail.com>
2026-02-20 09:31:16 +0100

    Styling adjustments

--- src/components/Blog/Roles.tsx
+++ src/components/Blog/Roles.tsx
@@ -1,12 +1,12 @@
 const Roles = ({ roles }: { roles: string[] }) => (
   <div className="my-5 py-2 border-body-color/20 border-t border-b">
-    <h2 className="text-xs text-body-color md:text-sm">
+    <h2 className="text-base text-body-color md:text-md">
       Main Roles</h2>
     <ul className="space-y-1 text-lg">
       {roles.map((item, index) => (
         <li
           key={index}
-          className={"text-sm text-black dark:text-white sm:text-md md:text-base"}
+          className={"text-base text-black dark:text-white sm:text-md md:text-lg"}
         >
           {item}
         </li>
--- src/components/Blog/Statistics.tsx
+++ src/components/Blog/Statistics.tsx
@@ -9,31 +9,31 @@ const Statistics = ({ stat }: { stat: Stat}) => {
         <div className="lg:flex items-center">
           <div className="border-body-color/20 items-center lg:border-r lg:pr-5 pb-3 lg:pb-0">
             <p className="text-xs text-body-color md:text-sm">{orgaType}</p>
-            <h4 className="text-sm text-black dark:text-white sm:text-md md:text-base">
+            <h4 className="text-sm text-black dark:text-white md:text-base">
               {orgaName}
             </h4>
           </div>
           <div className="border-body-color/20 items-center lg:border-r lg:px-5 pb-3 lg:pb-0">
             <p className="text-xs text-body-color md:text-sm">Game</p>
-            <h4 className="text-sm text-black dark:text-white sm:text-md md:text-base">
+            <h4 className="text-sm text-black dark:text-white md:text-base">
               {game}
             </h4>
           </div>
           <div className="border-body-color/20 items-center lg:border-r lg:px-5 pb-3 lg:pb-0">
             <p className="text-xs text-body-color md:text-sm">Date</p>
-            <h4 className="text-sm text-black dark:text-white sm:text-md md:text-base">
+            <h4 className="text-sm text-black dark:text-white md:text-base">
               {date}
             </h4>
           </div>
           <div className="border-body-color/20 items-center lg:border-r lg:px-5 pb-3 lg:pb-0">
             <p className="text-xs text-body-color md:text-sm">Duration</p>
-            <h4 className="text-sm text-black dark:text-white sm:text-md md:text-base">
+            <h4 className="text-sm text-black dark:text-white md:text-base">
               {duration}
             </h4>
           </div>
           <div className="border-body-color/20 items-center lg:px-5 pb-4 lg:pb-0">
             <p className="text-xs text-body-color md:text-sm">Team Size</p>
-            <h4 className="text-sm text-black dark:text-white sm:text-md md:text-base">
+            <h4 className="text-sm text-black dark:text-white md:text-base">
               {teamSize}
             </h4>
           </div>
