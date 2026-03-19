70909f2f3cc61ad84992c292bb8a98ef099c6675
Fabio Mangiameli <fabio.mangiameli22@gmail.com>
2026-03-19 13:47:32 +0100

    Project cleanup, deleted old template files which are not in use

--- src/components/Common/SectionTitle.tsx
+++
@@ -1,5 +0,0 @@
-const SectionTitle = ({ children }: { children: React.ReactNode }) => (
-  <h2 className="border-body-color/20 border-b mb-4 md:mb-6 pb-4 md:pb-6 text-xl font-bold leading-tight text-black dark:text-white sm:text-2xl sm:leading-tight md:text-3xl md:leading-tight">
-    {children}
-  </h2>
-);
